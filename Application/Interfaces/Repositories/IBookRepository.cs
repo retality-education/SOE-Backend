d7d34b4c66a2fc7f33daf2352182841ff7b56ce0
Vitalii Kertanov <RetalityAWD@yandex.ru>
2025-08-24 20:39:29 +0300

    hotfix catalog

---
+++ Application/Interfaces/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using Core.DTOs;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<PagedResult<Book>> GetBooksPagedAsync(BookFilter bookFilter, int page = 1, int pageSize = 32);
+    }
+}
